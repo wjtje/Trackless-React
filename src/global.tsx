bed0a608818e78f55bce804aee9dc20a27a7ba46
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2020-06-22 19:50:11 +0200

    Using SemVer

--- src/global.tsx
+++ src/global.tsx
@@ -8,7 +8,7 @@ export const auth = {
     Authorization: `Bearer ${apiKey}`
   }
 }
-export const version = "0.1.0";
+export const version = "0.1.1";
 export const NewInThisVersion = () => {
   return (
     <ul>
